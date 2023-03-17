86ff1d5acefe239446c86bf4b6484b348ceeed24
kvolchok <kk.volchok@gmail.com>
2023-03-17 17:53:47 +0300

    feat: create simple calculator

--- GitCalculator/Program.cs
+++ GitCalculator/Program.cs
@@ -1,3 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace GitCalculator;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Первое число.");
+            var firstNumber = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Математическая операция:\n +\n -\n *\n /\n");
+            var operation = Console.ReadLine();
+            
+            Console.WriteLine("Второе число.");
+            var secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            double result;
+
+            switch (operation)
+            {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case "/":
+                    result = firstNumber / secondNumber;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                default:
+                    Console.WriteLine("Выбрана неверная опреация.");
+                    break;
+            }
+        }
+    }
+}
